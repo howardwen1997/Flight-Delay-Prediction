4797471c2a46cf54f87c637f506290beccea28fc
Sam Penders <sam.penders10@gmail.com>
2023-04-20 14:51:57 -0500

    Save name for plot and csv, loop over layer sizes

--- models/train_mlp.py
+++ models/train_mlp.py
@@ -12,10 +12,6 @@ import pandas as pd
 # hyperparams
 #######################
 batchsize = 16
-learning_rate = 1e-4
-max_epochs = 200
-pos_weight = 3 # 3x as many not-delayed flights
-patience_time = 20 # early stopping
 #######################
 
 # get data
@@ -42,110 +38,136 @@ n_validation = validation_dataset.__len__()
 n_test = test_dataset.__len__()
 #######################
 
+# Training params
+#######################
+learning_rate = 1e-4
+max_epochs = 2
+pos_weight = 3 # 3x as many not-delayed flights
+patience_time = 20 # early stopping
+#######################
+
 # model architecture
-layers = [20, 20]
-dropout=False
+layer_set = [
+    [20, 20],
+    [50, 50],
+    [100, 100],
+    [20, 20, 20],
+    [50, 50, 50],
+    [100, 100, 100],
+    [200, 200, 200],
+]
+             
+dropouts=[False, True]
 
-model_name = 'weights/layers'
-for layer in layers:
-    model_name +=  '_' + str(layer)
-if dropout:
-    model_name += '_drop'
-model_name += '.pt'
-
-##############################
-
-mlp = FlightMLP(layers, dropout=dropout)
-criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(pos_weight), reduction='sum')
-optimizer = torch.optim.SGD(params=mlp.parameters(), lr=learning_rate)
-
-epoch_list = np.arange(1, max_epochs+1)
-train_losses = np.zeros(max_epochs)
-valid_losses = np.zeros(max_epochs)
-weights = [] # save weights
-
-for epoch in range(max_epochs):
-    correct = 0
-    for i, batch in enumerate(iter(train_dataloader)):
-        X = batch['features'].float()
-        labels = batch['delayed'].flatten()
-        
-        logit_pred = mlp.forward(X).flatten()
-        labels_pred = torch.sigmoid(logit_pred).round()
-        
-        optimizer.zero_grad()
-        loss = criterion(logit_pred, labels)
-        loss.backward()
-        optimizer.step()
-        
-        train_losses[epoch] += loss.item()
-        correct += (labels_pred == labels).sum()
-   
-    train_losses[epoch] = train_losses[epoch]/n_train 
+for layers in layer_set:
     
-    with torch.no_grad():
-        valid_labels = torch.tensor(validation_dataset.data[:,-1])
-        valid_pred_logit = mlp(torch.tensor(validation_dataset.data[:, :-1]).float()).round().reshape(-1)
-        valid_loss = criterion(valid_pred_logit, valid_labels)
-        valid_losses[epoch] = valid_loss.item()/n_validation
-    
-    # save weights
-    weights.append(mlp.state_dict())
-    weights = weights[-patience_time:]
+    for dropout in dropouts:
 
-    if epoch > patience_time:
-        if valid_losses[epoch] > valid_losses[epoch - patience_time]:
-            print('Stopping early\n')
+        save_name = 'layers'
+        for layer in layers:
+            save_name +=  '_' + str(layer)
+        if dropout:
+            save_name += '_drop'
+
+        model_name ='weights/' + save_name + '.pt'
+        loss_filename = '../results/loss' + save_name + '.csv'
+        plot_filename = '../plots/' + save_name + '.png'
+
+        ##############################
+
+        mlp = FlightMLP(layers, dropout=dropout)
+        criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(pos_weight), reduction='sum')
+        optimizer = torch.optim.SGD(params=mlp.parameters(), lr=learning_rate)
+
+        epoch_list = np.arange(1, max_epochs+1)
+        train_losses = np.zeros(max_epochs)
+        valid_losses = np.zeros(max_epochs)
+        weights = [] # save weights
+
+        for epoch in range(max_epochs):
+            correct = 0
+            for i, batch in enumerate(iter(train_dataloader)):
+                X = batch['features'].float()
+                labels = batch['delayed'].flatten()
+                
+                logit_pred = mlp.forward(X).flatten()
+                labels_pred = torch.sigmoid(logit_pred).round()
+                
+                optimizer.zero_grad()
+                loss = criterion(logit_pred, labels)
+                loss.backward()
+                optimizer.step()
+                
+                train_losses[epoch] += loss.item()
+                correct += (labels_pred == labels).sum()
+        
+            train_losses[epoch] = train_losses[epoch]/n_train 
+            
+            with torch.no_grad():
+                valid_labels = torch.tensor(validation_dataset.data[:,-1])
+                valid_pred_logit = mlp(torch.tensor(validation_dataset.data[:, :-1]).float()).round().reshape(-1)
+                valid_loss = criterion(valid_pred_logit, valid_labels)
+                valid_losses[epoch] = valid_loss.item()/n_validation
+            
+            # save weights
+            weights.append(mlp.state_dict())
+            weights = weights[-patience_time:]
+
+            if epoch > patience_time:
+                if valid_losses[epoch] > valid_losses[epoch - patience_time]:
+                    print('Stopping early\n')
+                    torch.save(weights[0], model_name)
+                    break
+
+            # print test set metrics
+            if epoch % 25 == 0:
+                with torch.no_grad():
+                    test_label = test_dataset.data[:, -1]
+                    test_pred = torch.sigmoid(mlp(torch.tensor(test_dataset.data[:, :-1]).float())).round().detach().numpy()
+
+                    precision, recall, f1, support = precision_recall_fscore_support(test_label, test_pred)
+                    accuracy = accuracy_score(test_label, test_pred)
+
+                    print('precision: ', precision[1])
+                    print('recall: ', recall[1])
+                    print('f1: ', f1[1])
+                    print('accuracy', accuracy)
+            
+            print(f'Epoch {epoch_list[epoch]}: loss: {train_losses[epoch]:>7f} accuracy: {correct/n_train:>7f} Val loss: {valid_losses[epoch]:.7f}')
+            
             torch.save(weights[0], model_name)
-            break
 
-    # print test set metrics
-    if epoch % 25 == 0:
-        with torch.no_grad():
-            test_label = test_dataset.data[:, -1]
-            test_pred = torch.sigmoid(mlp(torch.tensor(test_dataset.data[:, :-1]).float())).round().detach().numpy()
+        # test metrics
+        test_label = test_dataset.data[:, -1]
+        test_pred = torch.sigmoid(mlp(torch.tensor(test_dataset.data[:, :-1]).float())).round().detach().numpy()
 
-            precision, recall, f1, support = precision_recall_fscore_support(test_label, test_pred)
-            accuracy = accuracy_score(test_label, test_pred)
+        precision, recall, f1, support = precision_recall_fscore_support(test_label, test_pred)
+        accuracy = accuracy_score(test_label, test_pred)
 
-            print('precision: ', precision[1])
-            print('recall: ', recall[1])
-            print('f1: ', f1[1])
-            print('accuracy', accuracy)
-    
-    print(f'Epoch {epoch_list[epoch]}: loss: {train_losses[epoch]:>7f} accuracy: {correct/n_train:>7f} Val loss: {valid_losses[epoch]:.7f}')
-       
-    torch.save(weights[0], model_name)
+        print('precision: ', precision[1])
+        print('recall: ', recall[1])
+        print('f1: ', f1[1])
+        print('accuracy', accuracy)
 
-# test metrics
-test_label = test_dataset.data[:, -1]
-test_pred = torch.sigmoid(mlp(torch.tensor(test_dataset.data[:, :-1]).float())).round().detach().numpy()
+        # save values
+        train_losses = train_losses[:epoch+1]
+        valid_losses = valid_losses[:epoch+1]
+        epoch_list = epoch_list[:epoch+1]
 
-precision, recall, f1, support = precision_recall_fscore_support(test_label, test_pred)
-accuracy = accuracy_score(test_label, test_pred)
+        df = pd.DataFrame(
+            {'Epoch' : epoch_list,
+            'Train_loss' :  train_losses,
+            'val_loss' : valid_losses
+            }
+        )
+        df.to_csv(loss_filename, index=False)
 
-print('precision: ', precision[1])
-print('recall: ', recall[1])
-print('f1: ', f1[1])
-print('accuracy', accuracy)
-
-# save values
-train_losses = train_losses[:epoch+1]
-valid_losses = valid_losses[:epoch+1]
-epoch_list = epoch_list[:epoch+1]
-
-df = pd.DataFrame(
-    {'Epoch' : epoch_list,
-    'Train_loss' :  train_losses,
-    'val_loss' : valid_losses
-    }
-)
-df.to_csv('results/losses.csv', index=False)
-
-# plot loss
-plt.plot(epoch_list, train_losses, label='training loss')
-plt.plot(epoch_list, valid_losses, label='validation loss')
-plt.xlabel('Epoch')
-plt.ylabel('Avg. Loss')
-plt.legend()
-plt.savefig('../plots/mlp_loss.png')
+        # plot loss
+        fig, ax = plt.subplots()
+        ax.plot(epoch_list, train_losses, label='training loss')
+        ax.plot(epoch_list, valid_losses, label='validation loss')
+        ax.set_xlabel('Epoch')
+        ax.set_ylabel('Avg. Loss')
+        ax.legend()
+        fig.savefig(plot_filename)
+        fig.clear()
